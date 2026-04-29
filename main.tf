860299d46b341058434673daeafff02fe06c82b9
fraze-dev <aaron.fraze3@gmail.com>
2026-04-29 17:05:39 -0400

    fix: allow ssh from jenkins

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}/32, "172.31.0.0/16"]
+    cidr_blocks = ["${var.my_ip}/32", "172.31.0.0/16"]
   }
 
   egress {
